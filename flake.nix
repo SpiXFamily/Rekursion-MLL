ad04ef3ed2372b8404483430eebb00530a4ccea3
Vieta <94648307+yVieta@users.noreply.github.com>
2024-11-15 15:43:40 +0100

    nix flake0

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "MATHE MLL FLAKE";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
  outputs = { self , nixpkgs ,... }: let
